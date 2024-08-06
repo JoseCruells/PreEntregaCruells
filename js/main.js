3c3a558453c6ab591842e4463bb3186639bff0fb
Jose Cruells <jose.cruells0@gmail.com>
2024-08-06 16:21:11 -0300

    origin

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 function computeLoan() {
-    const loanAmount = parseFloat(document.getElementById('amount').value);
-    const interestRate = parseFloat(document.getElementById('interest_rate').value) / 100;
-    const months = parseInt(document.getElementById('months').value);
-  
-    
-    const monthlyInterest = interestRate / 12;
-    const numerator = monthlyInterest * Math.pow(1 + monthlyInterest, months);
+  const loanAmount = parseFloat(document.getElementById('amount').value);
+  const interestRate = parseFloat(document.getElementById('interest_rate').value) / 100;
+  const months = parseInt(document.getElementById('months').value);
+
+  const monthlyInterest = interestRate / 12;
+  let totalPayment = 0;
+
+  for (let i = 0; i < months; i++) {
+    const numerator = monthlyInterest * Math.pow(1 + monthlyInterest, months - i);
     const denominator = Math.pow(1 + monthlyInterest, months) - 1;
     const monthlyPayment = loanAmount * (numerator / denominator);
-  //formula para calcular el pago mensual
-  
-    document.getElementById('payment').textContent = `Monthly Payment: $${monthlyPayment.toFixed(2)}`;//esto mostraria el resultado
+    totalPayment += monthlyPayment;
   }
-  
+
+  document.getElementById('payment').textContent = `Total Payment: $${totalPayment.toFixed(2)}`;
+}
